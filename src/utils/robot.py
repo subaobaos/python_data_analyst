5c3336bcdfe06c40c85440655df2aa1aa8ddc6d7
wangguoqiang <737029580@qq.com>
2019-09-19 15:02:23 +0800

    add dingding_robot

--- src/utils/robot.py
+++ src/utils/robot.py
@@ -26,3 +26,59 @@
 #                                      //.-~~~--\
 #               神兽保佑
 #                  永无BUG!
+
+import requests
+from src.utils import aes
+from src.config import config
+
+encryption_instance = aes.Aestion()
+token = encryption_instance.decode_data(config.dingding_token)
+
+def dingding_robot(content=''):
+
+    try:
+
+        data = {
+            'msgtype': 'text',
+            'text': {
+                'content': content
+            },
+            'isAtAll': True
+        }
+
+        url = 'https://oapi.dingtalk.com/robot/send?access_token=' + token
+
+        requests.post(url, json=data)
+
+        print('钉钉消息推送成功')
+
+    except BaseException:
+
+        print('钉钉消息推送失败')
+
+        raise
+
+
+if __name__ == '__main__':
+
+    imag = 'https://subaobao-newdata.oss-cn-shanghai.aliyuncs.com/newdatabase/image/0072Vf1pgy1foxk7h6p4zj31hc0u0asb.jpg'
+
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=' + token
+
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": "标题",
+            "text": "#### 标题\n " +
+            "> test text\n\n" +
+            "![screenshot](https://subaobao-newdata.oss-cn-shanghai.aliyuncs.com/newdatabase/image/0072Vf1pgy1foxk7h6p4zj31hc0u0asb.jpg)",
+        }}
+
+    headers = {
+        'User-Agent': 'user_agent',
+        'Content-type': 'application/json'
+    }
+
+    rece = requests.post(url, json=data)
+
+    print(rece.text)
