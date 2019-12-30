57ded366fd18911f861772fa49fb18e6041e6332
小苏 <737029580@qq.com>
2019-12-30 13:05:07 +0800

    update

--- src/utils/xun_proxy.py
+++ src/utils/xun_proxy.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# @Time    : 2019-10-24 21:25
+# @Time    : 2019-12-25 14:05
 # @Author  : 小苏
 # @Email   : 737029580@qq.com
-# @File    : replace_json.py
+# @File    : xun_proxy.py
 # @Software: PyCharm
 
 #                                                    __----~~~~~~~~~~~------___
@@ -27,38 +27,26 @@
 #               神兽保佑
 #                  永无BUG!
 
-
-import re
-
-#### 在下面输入headers信息
-
-headers_str = """
-Host: 103.99.210.71:1111
-Connection: keep-alive
-Pragma: no-cache
-Cache-Control: no-cache
-Upgrade-Insecure-Requests: 1
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
-Accept-Encoding: gzip, deflate
-Accept-Language: zh-CN,zh;q=0.9
-Cookie: PHPSESSID=2ktl335nb357va4lo25eml48m5
-"""
-pattern = r"^(.*?):\s?(.*?$)"
-
-for line in headers_str.splitlines():
-    print(re.sub(pattern, r'"\1": "\2",', line))
+from config import config
+import requests
+import json
 
 
-#### 输出样例
+class XunProxy():
 
-# "Host": "103.99.210.71:1111",
-# "Connection": "keep-alive",
-# "Pragma": "no-cache",
-# "Cache-Control": "no-cache",
-# "Upgrade-Insecure-Requests": "1",
-# "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
-# "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-# "Accept-Encoding": "gzip, deflate",
-# "Accept-Language": "zh-CN,zh;q=0.9",
-# "Cookie": "PHPSESSID=2ktl335nb357va4lo25eml48m5",
+    def __init__(self):
+        self.spiderId = config.xun_proxy['spiderId']
+        self.order_number = config.xun_proxy['order_number']
+
+    def start(self):
+        url = "http://api.xdaili.cn/xdaili-api//greatRecharge/getGreatIp?spiderId=" + self.spiderId + "&orderno=" + self.order_number + "&returnType=2&count=1"
+        rece = requests.get(url)
+        rece = rece.json()
+        print(res)
+        port = rece['RESULT'][0]['port']
+        ip = rece['RESULT'][0]['ip']
+        return ip, port
+
+
+    def switch_proxy(self):
+        pass
